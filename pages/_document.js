69ea9d56955a772e5d97d5f1aa0c757de1ff2517
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-15 16:46:39 +0700

    GA

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ class MyDocument extends Document {
         <Head>
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=G-TZVPLHC3BT`}
           />
           <script
             dangerouslySetInnerHTML={{
@@ -33,7 +33,7 @@ class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', 'G-TZVPLHC3BT', {
               page_path: window.location.pathname,
             });
           `,
