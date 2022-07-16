36f8f9af0557d3de18bd8d315af09edad55f1fa7
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-16 16:36:29 +0700

    SSR

--- pages/mangas/[id].js
+++ pages/mangas/[id].js
@@ -1,28 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 import MangaCard from "@/layouts/MangaCard";
-export default function MangaId() {
-  const router = useRouter();
-  const [manga, setManga] = useState([]);
-  const searchByid = async () => {
-    if (router.query.id) {
-      const url = `https://api.jikan.moe/v3/manga/${router.query.id}`;
-      const response = await fetch(url);
-      const json = await response.json();
-      console.log(json);
-      setManga(json);
-    }
-  };
-  useEffect(() => {
-    searchByid();
-  }, [router]);
+import { GetServerSideProps } from "next";
+import Site_Head from "@/layouts/Site_Head";
+export default function MangaId({ manga }) {
   return (
-    <div>
+    <>
+      <Site_Head title={manga.title}>
+        <meta property="og:title" content={manga.title} />
+      </Site_Head>
       <div className="grid  grid-flow-row gap-4 justify-center items-center">
         <div key={manga.mal_id} className="gap-4">
           <MangaCard manga={manga}></MangaCard>
         </div>
       </div>
-    </div>
+    </>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  console.log("Server side props...", context.query);
+  const url = `https://api.jikan.moe/v3/manga/${context.query.id}`;
+  const response = await fetch(url);
+  const json = await response.json();
+  return { props: { manga: json } };
+};
